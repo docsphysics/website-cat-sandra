4c9a8ba1c8b844aec0905a033786b30aeb8d6041
docsphysics <91302700+docsphysics@users.noreply.github.com>
2021-10-20 15:57:04 +0800

    "First commit"

---
+++ script/myScript.js
@@ -0,0 +1,11 @@
+/* Navbar Javascript */
+
+/* Set the width of the sidebar to 100% open (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+}
+
+/* Set the width of the sidebar to 0 when closed (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
